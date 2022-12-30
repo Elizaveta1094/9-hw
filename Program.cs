2482f68dbda594e9ed4c736c58a108267572aa22
Elizaveta1094 <treninskaya.elizaveta@yandex.ru>
2022-12-30 13:48:24 +0300

    hw9

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 68
+
+int Rec(int m, int n)
+{
+if(m == 0)
+    return n+1;
+    if (n == 0 && m>0)
+    return Rec(m-1,1);
+        else
+        return Rec(m-1, Rec(m, n-1));
+}
+
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Rec(m,n);
+Console.WriteLine($"Аккерман ({m},{n}) = {Rec(m, n)}");
